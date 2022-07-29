7ec355069a0b1d5d421688ea28f6014d780dc743
danieldz21 <daniel.da.silva.tj2011@gmail.com>
2022-07-29 13:33:40 -0300

    useRef

--- src/components/TimerForm/index.js
+++ src/components/TimerForm/index.js
@@ -1,6 +1,10 @@
+import { useRef } from 'react';
+
 import style from './TimerForm.module.css';
 
 const TimerForm = ({ setTimers }) => {
+  const nameInputRef = useRef(null);
+
   const handleFormSubmit = (event) => {
     event.preventDefault();
 
@@ -12,23 +16,26 @@ const TimerForm = ({ setTimers }) => {
     setTimers((timers) => [...timers, newTimer]);
 
     form.reset();
+
+    nameInputRef.current.focus();
   }
 
   return (
     <form className={style.form} onSubmit={handleFormSubmit}>
       <input
-      type='text'
-      placeholder='Nome'
-      name='name'
-      className={style.input}
+        ref={nameInputRef}
+        type='text'
+        placeholder='Nome'
+        name='name'
+        className={style.input}
       />
 
       <input
-      type='number'
-      placeholder='Duração (segundos)
-      'name='duration'
-      className={style.input}
-      min='1'
+        type='number'
+        placeholder='Duração (segundos)'
+        name='duration'
+        className={style.input}
+        min='1'
       />
 
       <input type='submit' className={style.submitButton} value='+' />
